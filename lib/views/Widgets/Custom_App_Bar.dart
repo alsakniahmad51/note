54de425a467abfcf0b0cbe6ede0bd6240dd85207
Ahmad <you@example.com>
2023-06-10 23:45:55 +0300

    Custom App Bar

---
+++ lib/views/Widgets/Custom_App_Bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'Custom_Icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 30),
+        ),
+        Spacer(),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
