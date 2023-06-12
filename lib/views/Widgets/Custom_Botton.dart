55068df904528e650baab4e189e6588fc587e498
Ahmad <you@example.com>
2023-06-12 19:54:28 +0300

    Finish Note Botton Sheet

---
+++ lib/views/Widgets/Custom_Botton.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../Constants.dart';
+
+class CustomBotton extends StatelessWidget {
+  const CustomBotton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16), color: KprimaryColor),
+    );
+  }
+}
