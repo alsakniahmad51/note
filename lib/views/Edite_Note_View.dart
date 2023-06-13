674ee54f733532eabcc9b472613053626726fe3e
Ahmad <you@example.com>
2023-06-13 20:28:53 +0300

    create Edite Note View

---
+++ lib/views/Edite_Note_View.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Widgets/Edite_Note_View_Body.dart';
+
+class EditeNoteView extends StatelessWidget {
+  const EditeNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EditeNoteBody();
+  }
+}
---
+++ lib/views/Widgets/Edite_Note_View_Body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditeNoteBody extends StatelessWidget {
+  const EditeNoteBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
