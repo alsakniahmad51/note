7b807326e424dd70d2495e5753eb8221ab21b189
Ahmad <you@example.com>
2023-06-17 21:09:20 +0300

    change add note Loading

--- lib/views/Widgets/Add_Note_Form.dart
+++ lib/views/Widgets/Add_Note_Form.dart
@@ -45,20 +45,25 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 55,
           ),
-          CustomBotton(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                var notemodel = NoteModel(
-                    title: title!,
-                    subtitle: subTitle!,
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addNtoe(notemodel);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomBotton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var notemodel = NoteModel(
+                        title: title!,
+                        subtitle: subTitle!,
+                        date: DateTime.now().toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNtoe(notemodel);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
           )
         ],
--- lib/views/Widgets/Custom_Botton.dart
+++ lib/views/Widgets/Custom_Botton.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import '../../Constants.dart';
 
 class CustomBotton extends StatelessWidget {
-  const CustomBotton({super.key, this.onTap});
+  const CustomBotton({super.key, this.onTap, this.isLoading = false});
   final void Function()? onTap;
+  final isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -12,15 +13,25 @@ class CustomBotton extends StatelessWidget {
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 55,
-        child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-        ),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16), color: kPrimaryColor),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+        ),
       ),
     );
   }
