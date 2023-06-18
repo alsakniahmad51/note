e95f0d6e8e9e4e14bda7380f1f8a31da1a9ce112
Ahmad <you@example.com>
2023-06-18 19:10:06 +0300

    delete note done

--- lib/views/Widgets/Custom_Note_Item.dart
+++ lib/views/Widgets/Custom_Note_Item.dart
@@ -27,7 +27,9 @@ class NoteItem extends StatelessWidget {
             children: [
               ListTile(
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                  },
                   icon: const Icon(FontAwesomeIcons.trash, color: Colors.black),
                 ),
                 title: Text(
